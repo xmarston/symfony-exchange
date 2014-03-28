b9def32e0b4ac7bfaf1071afb4645ba1eaaf60ef
Daniel Guerrero <danielguerrero9@hotmail.com>
2014-03-29 00:03:47 +0100

    GeneralContentBundle
    Creación de GeneralContentBundle y modificación en UsuarioBundle

--- src/General/GeneralContentBundle/Controller/DefaultController.php
+++ src/General/GeneralContentBundle/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Usuario\UsuarioBundle\Controller;
+namespace General\GeneralContentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('UsuarioBundle:Default:index.html.twig');
+        return $this->render('GeneralContentBundle:Default:index.html.twig');
     }
 }
---
+++ src/General/GeneralContentBundle/GeneralContentBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace General\GeneralContentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GeneralContentBundle extends Bundle
+{
+}
